4f6df155700b4fbae045b2eb33af06433f9a0ff4
corentinventejol <97588972+corentinventejol@users.noreply.github.com>
2023-03-29 09:50:14 +0200

    code pour prendre les infos dans l'api
    le titre de page ne change pas :-1:
    les option colors ne fonctionne pas :-1:
    l'image ne s'affiche pas :-1:
    le nom s'affiche sur la page :+1:
    le prix s'affiche :+1:
    la description s'affiche :+1:
    et le altTxt ne fonctionne pas vu qu'il n'y a pas d'image :-1:

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,42 @@
+// Obtenez les paramètres de recherche de l'URL
+const params = new URLSearchParams(window.location.search);
 
+// Obtenez l'identifiant du produit à partir des paramètres de recherche
+const productId = params.get('id');
 
+// Utilisez l'API pour obtenir les informations sur le produit
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then(response => response.json())
+  .then(data => {
+    // Récupérez les informations du produit de la réponse de l'API
+    const { colors, _id, name, price, imageUrl, description, altTxt } = data;
 
+    // Changez le titre de la page avec le nom du produit
+    document.title = name;
+
+    // Mettez l'image dans la div avec la classe "item__img"
+    const itemImg = document.querySelector('.item__img');
+    itemImg.setAttribute('src', imageUrl);
+    itemImg.setAttribute('alt', altTxt);
+
+    // Mettez le nom du produit dans une balise h1
+    const productName = document.querySelector('h1');
+    productName.textContent = name;
+
+    // Mettez le prix du produit dans une balise span avec l'id "price"
+    const productPrice = document.querySelector('#price');
+    productPrice.textContent = price;
+
+    // Mettez la description du produit dans une balise p avec l'id "description"
+    const productDescription = document.querySelector('#description');
+    productDescription.textContent = description;
+
+    // Mettez les couleurs disponibles dans la balise option
+    const colorOptions = document.querySelector('#color-options');
+    colors.forEach(color => {
+      const option = document.createElement('option');
+      option.textContent = color;
+      colorOptions.appendChild(option);
+    });
+  })
+  .catch(error => console.error(error));
